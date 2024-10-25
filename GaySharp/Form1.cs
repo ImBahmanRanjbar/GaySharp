abc773afce700ff79e6a4852563b3da14e7cc432
Bahman <imbahmanranjbar@gmail.com>
2024-10-25 17:36:18 +0330

    Add files via upload

---
+++ GaySharp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaySharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Really?","", MessageBoxButtons.YesNo,MessageBoxIcon.Hand);
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Location = new Point(r.Next(12, 89), r.Next(158, 256));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("GOODBYE");
+            Application.Exit();
+        }
+    }
+}
